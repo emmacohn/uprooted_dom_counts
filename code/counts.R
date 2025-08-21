b77f09f0fc844a536e14c02b8a4b078c32ade43d
Emma Cohn <ecohn@epi.org>
2025-08-21 17:11:44 -0400

    with excel output

--- code/counts.R
+++ code/counts.R
@@ -77,6 +77,8 @@ cps_org <- load_org(2024,year, month, age, female, wbhao, union, educ, cow1, cit
     ),
   )
 
+### DOMESTIC WORKERS ###
+
 ## domestic (all) by region and state ##
 dom_geo <- cps_basic |> 
     filter(dom_work_ind == 1) |>
@@ -85,6 +87,24 @@ dom_geo <- cps_basic |>
         .by=c(statefips, region)) |>
     mutate(region = to_factor(region),
            statefips = to_factor(statefips))
+
+
+## domestic (occs) by region ##
+dom_occ_geo <- cps_basic |> 
+    filter(dom_work_ind == 1) |>
+    summarise(total_emp = sum(emp * basicwgt/12, na.rm=TRUE),
+        n=n(),
+        .by=c(dom_work, region)) |>
+    mutate(dom_work = case_when(dom_work == 1 ~ "Maids", 
+                            dom_work == 2 ~ "Childcare nannies",
+                            dom_work == 3 ~ "Childcare ownhome",
+                            dom_work == 4 ~ "DCA not agency",
+                            dom_work == 5 ~ "DCA agency"),
+          region = to_factor(region))
+
+wb$add_worksheet(sheet = "Domestic workers count") $
+  add_data(x = dom_geo, start_col = 1) $
+  add_data(x = dom_occ_geo, start_col = 7)
 
 ## domestic (all) by demographics (race, gender, race*gender, nativity) by region ##
 #find emp by sex
@@ -124,6 +144,12 @@ dom_native <- cps_basic |>
         n=n(),
         .by=c(citistat, region)) 
 
+wb$add_worksheet(sheet = "Domestic workers demographics") $
+  add_data(x = dom_race, start_col = 1) $
+  add_data(x = dom_sex, start_col = 7) $
+  add_data(x = dom_race_sex, start_col = 13) $
+  add_data(x = dom_native, start_col = 19)
+
 ## domestic (all) median wage by region and state ##
 dom_wages <- cps_org |> 
   filter(dom_work_ind == 1) |>
@@ -138,18 +164,10 @@ dom_wages <- cps_org |>
         n=n(),
         .by=c(statefips, region))
 
-## domestic (occs) by region ##
-dom_occ_geo <- cps_basic |> 
-    filter(dom_work_ind == 1) |>
-    summarise(total_emp = sum(emp * basicwgt/12, na.rm=TRUE),
-        n=n(),
-        .by=c(dom_work, region)) |>
-    mutate(dom_work = case_when(dom_work == 1 ~ "Maids", 
-                            dom_work == 2 ~ "Childcare nannies",
-                            dom_work == 3 ~ "Childcare ownhome",
-                            dom_work == 4 ~ "DCA not agency",
-                            dom_work == 5 ~ "DCA agency"),
-          region = to_factor(region))
+wb$add_worksheet(sheet = "Domestic workers wages") $
+  add_data(x = dom_wages, start_col = 1)
+
+### AGRICULTURE ###
 
 ## ag by region and state ##
 ag_geo <- cps_basic |> 
@@ -159,6 +177,9 @@ ag_geo <- cps_basic |>
         .by=c(statefips, region)) |>
     mutate(region = to_factor(region),
            statefips = to_factor(statefips))
+
+wb$add_worksheet(sheet = "Agricultural workers count") $
+  add_data(x = ag_geo, start_col = 1)
 
 ## ag by demographics by region ##
 #find emp by sex
@@ -198,6 +219,12 @@ ag_native <- cps_basic |>
         n=n(),
         .by=c(citistat, region)) 
 
+wb$add_worksheet(sheet = "Ag workers demographics") $
+  add_data(x = ag_race, start_col = 1) $
+  add_data(x = ag_sex, start_col = 7) $
+  add_data(x = ag_race_sex, start_col = 13) $
+  add_data(x = ag_native, start_col = 19)
+
 ## ag wage by region and state ##
 ag_wages <- cps_org |> 
   filter(occ18 == 6050) |>
@@ -212,6 +239,11 @@ ag_wages <- cps_org |>
         n=n(),
         .by=c(statefips, region))
 
+wb$add_worksheet(sheet = "Ag workers wages") $
+  add_data(x = ag_wages, start_col = 1)
+
+### PUBLIC SECTOR ###
+
 ## pub sec (all) by region and state ##
 pubsec_all_geo <- cps_basic |> 
     filter(cow1 %in% c(1, 2, 3)) |>
@@ -220,6 +252,23 @@ pubsec_all_geo <- cps_basic |>
         .by=c(statefips, region)) |> 
       mutate(region = to_factor(region),
              statefips = to_factor(statefips))
+
+## pub sec (teachers) by region and state ##
+pubsec_ed_geo <- cps_basic |> 
+    filter(cow1 %in% c(1, 2, 3)) |>
+    mutate(pub_occ = case_when(occ18 %in% c(2300, 2310, 2320, 2330, 2360, 2545) ~ 1,
+         TRUE ~ 2),
+        pub_occ = case_when(pub_occ == 1 ~ "Teachers", 
+                            pub_occ == 2 ~ "Other pub sec"),
+        region = to_factor(region),
+        statefips = to_factor(statefips)) |>
+    summarise(total_emp = sum(emp * basicwgt/12, na.rm=TRUE),
+        n=n(),
+        .by=c(pub_occ, region, statefips))
+
+wb$add_worksheet(sheet = "Public sector workers count") $
+  add_data(x = pubsec_all_geo, start_col = 1) $
+  add_data(x = pubsec_ed_geo, start_col = 7)
 
 ## pub sec (all) by demographics by region ##
 #find emp by sex
@@ -259,6 +308,12 @@ pubsec_native <- cps_basic |>
         n=n(),
         .by=c(citistat, region)) 
 
+wb$add_worksheet(sheet = "Pub sec workers demographics") $
+  add_data(x = pubsec_race, start_col = 1) $
+  add_data(x = pubsec_sex, start_col = 7) $
+  add_data(x = pubsec_race_sex, start_col = 13) $
+  add_data(x = pubsec_native, start_col = 19)
+
 ## pub sec (all) wage by region and state ##
 pubsec_wages <- cps_org |> 
   filter(cow1 %in% c(1, 2, 3)) |>
@@ -273,21 +328,13 @@ pubsec_wages <- cps_org |>
         n=n(),
         .by=c(statefips, region))
 
-## pub sec (teachers) by region and state ##
-pubsec_ed_geo <- cps_basic |> 
-    filter(cow1 %in% c(1, 2, 3)) |>
-    mutate(pub_occ = case_when(occ18 %in% c(2300, 2310, 2320, 2330, 2360, 2545) ~ 1,
-         TRUE ~ 2),
-        pub_occ = case_when(pub_occ == 1 ~ "Teachers", 
-                            pub_occ == 2 ~ "Other pub sec"),
-        region = to_factor(region),
-        statefips = to_factor(statefips)) |>
-    summarise(total_emp = sum(emp * basicwgt/12, na.rm=TRUE),
-        n=n(),
-        .by=c(pub_occ, region, state))
+wb$add_worksheet(sheet = "Pub sec workers wages") $
+  add_data(x = pubsec_wages, start_col = 1)
+
+### ALL WORKERS ###
 
 ## all workers wage by region and state ##
-pubsec_wages <- cps_org |> 
+all_wages <- cps_org |> 
   mutate(region = to_factor(region),
         statefips = to_factor(statefips)) |> 
   summarise(
@@ -298,3 +345,9 @@ pubsec_wages <- cps_org |>
         quantiles_w = c(1:4, 5, 4:1)),
         n=n(),
         .by=c(statefips, region))
+
+wb$add_worksheet(sheet = "All workers wages") $
+  add_data(x = all_wages, start_col = 1)
+
+#save workbook
+wb_save(wb, "output/uprooted_counts_wages.xlsx")
